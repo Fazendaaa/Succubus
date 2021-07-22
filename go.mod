cc422b5e8f4370667ac5307a1b4e8d9175152e82
Fazendaaa <lucas.carotta@outlook.com>
2021-07-21 23:14:28 -0300

    Starting exec

--- go.mod
+++ go.mod
@@ -4,13 +4,12 @@ go 1.16
 
 require (
 	github.com/containerd/containerd v1.5.2 // indirect
-	github.com/docker/docker v20.10.6+incompatible // indirect
+	github.com/docker/docker v20.10.7+incompatible
 	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
-	github.com/spf13/cobra v1.2.1 // indirect
-	github.com/spf13/viper v1.8.1 // indirect
+	github.com/spf13/cobra v1.2.1
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	golang.org/x/text v0.3.6 // indirect
-	google.golang.org/grpc v1.38.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
--- pkg/engine.go
+++ pkg/engine.go
@@ -1,6 +1,18 @@
 package succubus
 
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
 type Docker struct {
+	client      *client.Client
+	containerID string
 }
 
 type LXC struct {
@@ -16,4 +28,66 @@ type RKT struct {
 }
 
 type Engine struct {
+	kind   string
+	docker Docker
+}
+
+func (docker *Docker) start() (ok bool, fail error) {
+	docker.client, fail = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+
+	if nil != fail {
+		return false, fmt.Errorf("%w;\ncould not start Docker connection", fail)
+	}
+
+	return ok, fail
+}
+
+func (engine *Engine) start(kind string) (ok bool, fail error) {
+	if "docker" != kind {
+		return ok, fmt.Errorf("engine '%s' is not supported yet", kind)
+	}
+
+	engine.kind = kind
+	engine.docker = Docker{}
+
+	return engine.docker.start()
+}
+
+func (docker *Docker) registryImage(image Image) (ok bool, fail error) {
+	ctx := context.Background()
+	reader, fail := docker.client.ImagePull(ctx, "docker.io/library/go", types.ImagePullOptions{})
+
+	if nil != fail {
+		return false, fmt.Errorf("%w;\nerror while fetching docker '%s' image", fail, image.name)
+	}
+
+	io.Copy(os.Stdout, reader)
+
+	return ok, fail
+}
+
+func (engine *Engine) registryImage(image Image) (ok bool, fail error) {
+	return engine.docker.registryImage(image)
+}
+
+func (docker *Docker) execute(command []Commands) (ok bool, fail error) {
+	ctx := context.Background()
+	response, fail := docker.client.ContainerExecCreate(ctx,
+		docker.containerID,
+		types.ExecConfig{
+			AttachStderr: true,
+			AttachStdout: true,
+			Cmd:          command[0].commands,
+		},
+	)
+
+	if nil != fail {
+		return ok, fmt.Errorf("%w;\nerror while trying to execute given '%s' command", fail, command[0].commands)
+	}
+
+	return ok, fail
+}
+
+func (engine *Engine) execute(command []Commands) (ok bool, fail error) {
+	return engine.docker.execute(command)
 }
